3bb01214f0e91fed2afd61bd324892fb7e5cb047
Abdallah Eid <abdallaheeid@icloud.com>
2025-11-25 23:06:42 +0100

    Encoding JWT

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,27 @@
 from passlib.context import CryptContext
-import hashlib
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+from jose import jwt, JWTError
+from models import User 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from database import SessionLocal
 
+SECRET_KEY = "55f6adb5826192bc65e739470ffd3a0a06b7076f41cd59c2b5cf4c06990152c4"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 def hash_password(password: str) -> str:
     """Hash a plain password using bcrypt."""
@@ -11,3 +31,56 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Check if a password matches the hash."""
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    """Create the JWT access token."""
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str):
+    """Decode a JWT token and return its payload."""
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    """Check if user exists and password matches."""
+    user = db.query(User).filter(User.username == username).first()
+
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_pass):
+        return False
+
+    return True
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    payload = decode_access_token(token)
+    email: str = payload.get("sub")
+
+    if email is None:
+        raise HTTPException(status_code=401, detail="Invalid token payload")
+
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
--- routers/auth_router.py
+++ routers/auth_router.py
@@ -1,10 +1,16 @@
+from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 from models import User
 from database import SessionLocal
 from typing_extensions import Annotated
-from .auth import hash_password
+from .auth import (
+    hash_password,
+    verify_password,
+    create_access_token,
+    get_current_user,
+    authenticate_user)
 
 router = APIRouter()
 
@@ -20,18 +26,17 @@ dp_dependency = Annotated[Session, Depends(get_db)]
 # -------------------- DTOs --------------------
 class UserCreate(BaseModel):
     username: str
-    email: EmailStr
+    email: str
     first_name: str 
     last_name: str
     password: str
     role: str
 
 class UserLogin(BaseModel):
-    email: EmailStr
+    username: str
     password: str
     
 
-# -------------------- Create User --------------------
 @router.post("/auth/register", status_code=status.HTTP_201_CREATED)
 async def register(user: UserCreate, db: dp_dependency):
 
@@ -57,4 +62,38 @@ async def register(user: UserCreate, db: dp_dependency):
         "message": "User registered successfully",
         "user_id": new_user.id,
         "email": new_user.email
+    }
+    
+
+@router.post("/login")
+async def login(data: UserLogin, db: Session = Depends(get_db)):
+
+    if not authenticate_user(db, data.username, data.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password"
+        )
+    
+    user = db.query(User).filter(User.username == data.username).first()
+
+    if not user.is_active:
+        raise HTTPException(status_code=403, detail="User is inactive")
+
+    token = create_access_token({"sub": user.username}, timedelta(minutes=20))
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
+
+
+@router.get("/me")
+async def get_me(current_user: User = Depends(get_current_user)):
+
+    return {
+        "id": current_user.id,
+        "username": current_user.username,
+        "email": current_user.email,
+        "name": f"{current_user.first_name} {current_user.last_name}",
+        "active": current_user.is_active
     }
